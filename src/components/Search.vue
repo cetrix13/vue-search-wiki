581a9b6f498340ced6fdf6c156e0cfcf8b466383
= <=>
2019-03-24 20:58:26 +0300

    Complete components Results and Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="Search">
     <input type="text" class="Search__input" v-model="query" />
-    <button class="Search__button">Search</button>
+    <button class="Search__button" @click="search()">Search</button>
   </div>
 </template>
 
 <script>
 export default {
-  data: function() {
+  data() {
     return {
       query: ""
-    };
+    }
+  },
+  methods: {
+    search() {
+      this.$store.dispatch('search', this.query)
+    }
   }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,32 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    results: []
+  },
+  getters: {
+    results(state) {
+      return state.results.map(item => {
+        item.url = 'https://ru.wikipedia.org/wiki/' + item.title;
+        return item;
+      })
+    }
+  },
+  mutations: {
+    set(state, {type, items}) {
+      state[type] = items;
+    }
+  },
+  actions: {
+    search: function ({commit}, query) {
+      const url = 'https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + query + '=&format=json';
+      const res = fetch(url, { method: "GET", mode: "cors" })
+        .then(function onFulfilled(res) {
+        return res.json();
+      }).catch(error => console.log(error));
+
+      const results = res.query.search;
+      commit('set', { type: 'results', items: results });
+    }
+  }
 });
